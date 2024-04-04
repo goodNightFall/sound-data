e367b44ae8e24b4cac10322bdbcabe3e2aa4cc74
goodNightFall <denissav203gmail.com>
2024-04-04 20:02:51 +0300

    Created a database and implemented models

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 require("dotenv").config()
 const express = require("express")
 const sequelize = require("./db")
+// eslint-disable-next-line no-unused-vars
+const models = require("./models/models")
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
+app.use(express.json())
 
 const start = async () => {
   try {
